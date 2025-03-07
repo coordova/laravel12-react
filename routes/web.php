8cd1961270513f08d2fe322c34fb60fb3fb5ef94
coordova <grcordova@gmail.com>
2025-03-07 19:03:29 -0400

    Post Model, Post Controller, Posts tsx,  Post page & route created

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+use App\Http\Controllers\PostController;
+
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
@@ -12,6 +14,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
+
+Route::resource('posts', PostController::class);
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
