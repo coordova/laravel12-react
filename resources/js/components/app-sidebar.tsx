374ab01aa310346ba7b9c5389637be4fe37af6ec
coordova <grcordova@gmail.com>
2025-03-18 19:44:44 -0400

    payment model / factory / seeder / controller created

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -18,6 +18,11 @@ const mainNavItems: NavItem[] = [
         url: '/posts',
         icon: FileText,
     },
+    {
+        title: 'Payments',
+        url: '/payments',
+        icon: FileText,
+    },
 ];
 
 const footerNavItems: NavItem[] = [
